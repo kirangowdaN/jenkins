e2b20fa7e9d4bba881fd5d153045437ef7c8885e
kiran <kiran@gmail.com>
2024-02-19 07:07:27 +0000

    added jenkins file

--- jenkins.sh
+++ jenkins.sh
@@ -1,16 +1,1 @@
-[200~duser="admin"
-dpasswd="secret"
-echo "enter username"
-read a
-echo "enter password"
-read b
-until [ $duser == $a ] && [ $dpasswd ==$b ];
-do
-        echo "invalid details"
-        echo "please enter valid details"
-        echo "enter username"
-        read a
-        echo "enter password"
-        read b
-done
-echo "sucessfully login"i
+echo "hello world"
